2bc52b4cd8474105b2df8f3764db9c01473331f3
pyli <pyli@ronglian.com>
2018-12-12 15:31:41 +0800

    增加注释

--- src/system/task.py
+++ src/system/task.py
@@ -5,6 +5,7 @@ Created on 2018年12月12日
 '''
 from configure import runTime
 
+#递归的从任务依赖表中查询当前任务名称对应的前序任务
 def findAllEarlierTask(taskName, res):
     if runTime.TASK_DEPEND_MAP[taskName]==[]:
         return res
@@ -13,7 +14,8 @@ def findAllEarlierTask(taskName, res):
             res.append(name)
             findAllEarlierTask(name, res)
         return res
-            
+    
+#获取任务列表的所有前序任务，保证是从最后到最前的顺序
 def getNecessaryTaskList(taskList):
     necessaryTaskList = []
     for taskName in taskList:
@@ -24,7 +26,7 @@ def getNecessaryTaskList(taskList):
             for necessaryTsakNeme in earlierTaskList:
                 if necessaryTsakNeme not in necessaryTaskList:
                     necessaryTaskList.append(necessaryTsakNeme)
-    print(necessaryTaskList)
+    return necessaryTaskList
     
 if __name__ == '__main__':
     getNecessaryTaskList(['wordSegment'])
